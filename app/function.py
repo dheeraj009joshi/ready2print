5d85b1f0090b4170fec6fb5d5ad8a6e15088a5f7
dheeraj009joshi <dlovej009@gmail.com>
2024-02-25 10:41:42 +0530

    readt to add commit

--- app/function.py
+++ app/function.py
@@ -1,10 +1,14 @@
 import time
+import ssl
 from functools import wraps
 from flask import session,redirect,url_for,request, render_template
 from bson.objectid import ObjectId
 from selenium import webdriver
 from app.DB.Db_config import collection
 from app.ONELOGIN_CLASS.ONELOGIN import onLogin
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 def login_required(func):
     @wraps(func)
@@ -47,7 +51,31 @@ def selenium_task():
     except Exception as e:
         # Handle exceptions and update status accordingly
         selenium_status = {'message': f'Selenium script failed: {str(e)}', 'progress': 100}
-        
+      
+def send_email(subject, body):
+    try:
+        sender_email = 'support@bixid.in'
+        sender_password = 'support@bixid'
+        recipient_email = 'j.b.fitterman@gmail.com'
+        msg = MIMEMultipart()
+        msg['From'] = sender_email
+        msg['To'] = recipient_email
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'plain'))
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL('mum2.hostarmada.net', 465, context=context) as server:
+            # server.starttls()
+            print(' email server started ')
+            server.login(sender_email, sender_password)
+            print(' email logged in  ')
+            server.sendmail(sender_email, recipient_email, msg.as_string())
+            data={"Status": "Success","Message":"Thanks for placing your request, our customer success team will ping you shortly"}
+            return  data 
+    except Exception as e:
+        data= {"Status": "Fail","Message":f"There is some error contact to our customer care for support {e}"}
+        return data 
+
+  
         
 def current_user():
     # Check if the user is logged in
@@ -70,12 +98,14 @@ def auto_email(all_urls,email_subject,email_body,user_id):
     for url in all_urls:
         print("i am for loop ")
         try:
+            send_email("Update From Tikun-Automation","Hi your Automatic Emails process is starting now will inform you when it's finished ")
             object.send_email(url,email_subject,email_body)
             user["todays_emails"] = user.get("todays_emails", 0) + 1
             result = collection.update_one({"_id": user_id}, {"$set": user})
         except Exception as err:
             print(err)
             pass
+    send_email("Update From Tikun-Automation","Hi your Automatic Emails process is Completed  now ")
 
 def auto_text(all_urls, message,user_id):
     user_id = ObjectId(str(user_id))
@@ -85,8 +115,10 @@ def auto_text(all_urls, message,user_id):
     object.open_eleads_oneLogin()
     for url in all_urls:
         try:
+            send_email("Update From Tikun-Automation","Hi your Automatic Message process is starting now will inform you when it's finished ")
             object.send_message(url,message)
             user["todays_emails"] = user.get("todays_emails", 0) + 1
             result = collection.update_one({"_id": user_id}, {"$set": user})
         except Exception as err:
             pass
+    send_email("Update From Tikun-Automation","Hi your Automatic Message process is completed now ")
