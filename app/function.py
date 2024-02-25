0f5b7026c210d623b977e028ccd81b93b0cfa71c
dheeraj009joshi <dlovej009@gmail.com>
2024-02-25 11:16:01 +0530

    added proper requirements.txt

--- app/function.py
+++ app/function.py
@@ -22,28 +22,7 @@ def login_required(func):
     return wrapper
     
 
-def send_email(subject, body):
-    try:
-        sender_email = 'support@bixid.in'
-        sender_password = 'support@bixid'
-        recipient_email = 'j.b.fitterman@gmail.com'
-        msg = MIMEMultipart()
-        msg['From'] = sender_email
-        msg['To'] = recipient_email
-        msg['Subject'] = subject
-        msg.attach(MIMEText(body, 'plain'))
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL('mum2.hostarmada.net', 465, context=context) as server:
-            # server.starttls()
-            print(' email server started ')
-            server.login(sender_email, sender_password)
-            print(' email logged in  ')
-            server.sendmail(sender_email, recipient_email, msg.as_string())
-            data={"Status": "Success","Message":"Thanks for placing your request, our customer success team will ping you shortly"}
-            return  data 
-    except Exception as e:
-        data= {"Status": "Fail","Message":f"There is some error contact to our customer care for support {e}"}
-        return data 
+
 
 
 def onelogin_login():
